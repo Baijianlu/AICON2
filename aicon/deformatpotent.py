a50b139c17e6a4a299b63131d5309328472cf22c
Tao Fan <48915805+Baijianlu@users.noreply.github.com>
2020-08-18 12:33:52 +0300

    Update deformatpotent.py
    Add helping message.

--- aicon/deformatpotent.py
+++ aicon/deformatpotent.py
@@ -9,9 +9,8 @@ from pymatgen.io.vasp import BSVasprun
 from pymatgen.electronic_structure.core import Spin
 
 class DeformatPotentC(object):
-    '''This class is used to calculate deformational potential constant and store the data  '''
+    ''' Deformational potential constant class '''
     
-    ###class method######################################################################
     def __init__(self, flag):
         self.value = 0.0
         self.flag = flag
@@ -26,13 +25,17 @@ class DeformatPotentC(object):
     
     __repr__ = __str__
 
-    def Get_bandstruct(self, filepath):                    #CBM,VBM SB could be merged
+    def Get_bandstruct(self, filepath):
+        ''' Calculate band structure '''
+        
         vaspband = BSVasprun(filepath + "vasprun.xml")
         bandstru = vaspband.get_band_structure(kpoints_filename=filepath+"KPOINTS",line_mode=True)
         return bandstru
            
     
     def DPCcalculator(self, filepath, *pos):
+        ''' Get the energy of the band edge at different volume. '''
+        
         bndindex = pos[0]
         kptindex = pos[1]
         for fi in filepath:
@@ -41,19 +44,23 @@ class DeformatPotentC(object):
                 
         
     def Get_Strain(self, filepath):
-        Vol = list()                                         # number of strain = number of files - 1
+        ''' Get the strain. '''
+        
+        Vol = list()                                                             # number of strain = number of files - 1
         for fi in filepath:
             vasprun = BSVasprun(fi + "vasprun.xml")
             Vol.append(vasprun.final_structure.volume)
             
         self.strain = [np.abs(Vol[i]-Vol[0])/Vol[0] for i in np.arange(1, len(Vol))]
-        return
+        
     
-    def Get_DPC(self, filepath, *pos):        
+    def Get_DPC(self, filepath, *pos):
+        ''' Calculate the deformaton potential constant. '''
+        
         self.DPCcalculator(filepath, *pos)
         self.Get_Strain(filepath)
         DPC = [np.abs(self.englist[i] - self.englist[0])/(self.strain[i-1]) for i in np.arange(1,len(self.englist))]
         self.value = np.average(DPC)
-        return DPC 
+         
         
         
