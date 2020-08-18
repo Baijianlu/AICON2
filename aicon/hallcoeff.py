6cd0950bb8e3d535a0dceb85db09df585d0e837f
Tao Fan <48915805+Baijianlu@users.noreply.github.com>
2020-08-18 12:01:41 +0300

    Update hallcoeff.py
    Add helping message.

--- aicon/hallcoeff.py
+++ aicon/hallcoeff.py
@@ -17,29 +17,50 @@ def Upperlimit(x):
         return x + 100
 
 class Hallcoeff(object):
-    '''This is a class for seebeck coefficient'''
+    '''Hall coefficient class'''
+    
     def __init__(self, flag, RelaxTime):
         self.value = 0.0
         self.flag = flag
         self.RelaxT = RelaxTime
         
     def Get_AK(self):
+        ''' Calculate effective mass anisotropy factor. '''
+        
         K = self.RelaxT.ACO.EMC.parallelmass / self.RelaxT.ACO.EMC.verticalmass
         self.A_K = 3 * K * (K + 2) / (2 * K + 1)**2
         
     def Get_A_NP(self):
+        ''' Calculate Hall factor with non-parabolic approximation.'''
+        
         fun1 = lambda z, x, T: 1e14 * self.Totaltime(z, T) * (-self.RelaxT.DfermidistrFun(z,x)) * self.RelaxT.ACO.Moment(z, T)**3 / (1 + 2 * self.RelaxT.Beta(T) * z)
         fun2 = lambda z, x, T: (1e14 * self.Totaltime(z, T))**2 * (-self.RelaxT.DfermidistrFun(z,x)) * self.RelaxT.ACO.Moment(z, T)**3 / (1 + 2 * self.RelaxT.Beta(T) * z)**2
         fun3 = lambda z, x, T: (-self.RelaxT.DfermidistrFun(z,x)) * self.RelaxT.ACO.Moment(z, T)**3
         self.A = lambda x, T: self.A_K * quad(fun2, 0, Upperlimit(x), args=(x, T))[0] * quad(fun3, 0, Upperlimit(x), args=(x, T))[0] / (quad(fun1, 0, Upperlimit(x), args=(x, T))[0])**2
 
     def Get_A_P(self):
+        ''' Calculate Hall factor with parabolic approximation. '''
+        
         fun1 = lambda z, x, T: 1e14 * self.Totaltime(z, T) * (-self.RelaxT.DfermidistrFun(z,x)) * self.RelaxT.ACO.Moment(z, T)**3
         fun2 = lambda z, x, T: (1e14 * self.Totaltime(z, T))**2 * (-self.RelaxT.DfermidistrFun(z,x)) * self.RelaxT.ACO.Moment(z, T)**3
         fun3 = lambda z, x, T: (-self.RelaxT.DfermidistrFun(z,x)) * self.RelaxT.ACO.Moment(z, T)**3
         self.A = lambda x, T: self.A_K * quad(fun2, 0, Upperlimit(x), args=(x, T))[0] * quad(fun3, 0, Upperlimit(x), args=(x, T))[0] / (quad(fun1, 0, Upperlimit(x), args=(x, T))[0])**2
 
     def Get_hallcoeff(self, density, ACO = True, ACO_P = False, OPT = False, OPT_P = False, IMP = False, IMP_P = False):
+        ''' 
+        Calculate Hall coefficient. 
+        
+        Parameters:
+        ----------
+        density: function
+            The carrier concentration function.
+        ACO: bool
+            If consider the acoustic phonon scattering in total relaxation time.
+        ACO_P: bool
+            If consider the acoustic phonon scattering with parabolic approximation in total relaxation time.
+            
+        '''
+        
         fun1 = lambda z, T: 0
         fun2 = lambda z, T: 0
         fun3 = lambda z, T: 0
