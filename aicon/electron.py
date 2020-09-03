742f481f7808e4695f372f145a36b96a75e4224b
Tao Fan <48915805+Baijianlu@users.noreply.github.com>
2020-09-03 12:50:07 +0300

    Add claim

--- aicon/electron.py
+++ aicon/electron.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 20 14:07:20 2020
+# Copyright (C) 2020 Tao Fan
+# All rights reserved.
+#
+# This file is part of AICON.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in
+#   the documentation and/or other materials provided with the
+#   distribution.
+#
+# * Neither the name of the AICON project nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
 
-@author: Tao.Fan
-"""
 import os
 import numpy as np
 from pymatgen.io.vasp import BSVasprun
